a98a247fd9e38af1fc1a1b474f38e8d320401134
Paul Choi <paulsoonminchoi@gmail.com>
2021-02-24 18:27:30 -0800

    Add default text for footer and background color change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,96 @@
 import React from 'react';
 import styled from 'styled-components';
 import { primaryFont, typeScale, colors } from '../utils/index';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(fab);
 
 const FooterContainer = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   height: 20vh;
-  background-color: ${colors.secondary3};
+  background: linear-gradient(180deg, rgba(219, 211, 201, 0) 0%, #dbd3c9 50%);
+  align-items: center;
+
+  > .section-column {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    > span {
+      margin: 10px;
+      font-family: ${primaryFont};
+      font-size: ${typeScale.subBodyText};
+      cursor: pointer;
+      text-align: center;
+    }
+  }
+`;
+
+const SectionColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  > span {
+    margin: 10px;
+    font-family: ${primaryFont};
+    font-size: ${typeScale.subBodyText};
+    cursor: pointer;
+    text-align: center;
+  }
+`;
+
+const SectionRow = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  > span {
+    margin: 10px;
+    font-family: ${primaryFont};
+    font-size: ${typeScale.subBodyText};
+    cursor: pointer;
+    text-align: center;
+  }
 `;
 
 const Footer: React.FC = () => {
-  return <FooterContainer>This is the Footer</FooterContainer>;
+  return (
+    <FooterContainer>
+      <SectionColumn>
+        <span>Testimonials</span>
+        <span>Careers</span>
+        <span>Investors</span>
+        <span>Terms of Service</span>
+      </SectionColumn>
+      <SectionColumn>
+        <span>How it works</span>
+        <span>Support</span>
+        <span>Contact</span>
+        <span>Giffy © 2021</span>
+      </SectionColumn>
+      <SectionRow>
+        <span>
+          <FontAwesomeIcon icon={['fab', 'instagram']} />
+        </span>
+        <span>
+          <FontAwesomeIcon icon={['fab', 'facebook']} />
+        </span>
+        <span>
+          <FontAwesomeIcon icon={['fab', 'twitter']} />
+        </span>
+        <span>
+          <FontAwesomeIcon icon={['fab', 'linkedin']} />
+        </span>
+        <span>
+          <FontAwesomeIcon icon={['fab', 'youtube']} />
+        </span>
+      </SectionRow>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
